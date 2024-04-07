3d8fdaa94d499fd4c64fded66bcd38cce73189bf
mukh4mmadov <omuhammadov467@gmail.com>
2024-04-07 10:57:51 +0500

    add

--- js/app.js
+++ js/app.js
@@ -1,55 +1,8 @@
 const projects = [
   {
-    title: "Animation-1",
-    githubLink: "https://github.com/mukh4mmadov/animation-1",
-    vercelLink: "https://animation-1-cyan.vercel.app/",
-  },
-  {
-    title: "Animation-2",
-    githubLink: "https://github.com/mukh4mmadov/animation-2",
-    vercelLink: "https://animation-2-six.vercel.app/",
-  },
-  {
-    title: "Animation-3",
-    githubLink: "https://github.com/mukh4mmadov/animation-3",
-    vercelLink: "https://animation-3-dvjo.vercel.app/",
-  },
-  {
-    title: "Animation-4",
-    githubLink: "https://github.com/mukh4mmadov/animation-4",
-    vercelLink: "https://animation-4-sage.vercel.app/",
-  },
-  {
-    title: "Animation-5",
-    githubLink: "https://github.com/mukh4mmadov/animation-5",
-    vercelLink: "https://animation-5-phi.vercel.app/",
-  },
-  {
-    title: "Animation-6",
-    githubLink: "https://github.com/mukh4mmadov/animation-6",
-    vercelLink: "https://animation-6-gold.vercel.app/",
-  },
-  {
-    title: "Animation-7",
-    githubLink: "https://github.com/mukh4mmadov/animation-7",
-    vercelLink: "https://animation-7-two.vercel.app/",
-  },
-  {
-    title: "Animation-8",
-    githubLink: "https://github.com/mukh4mmadov/animation-8",
-    vercelLink:
-      "https://animation-8-mgi9a0npq-ozodbeks-projects-5b942123.vercel.app/",
-  },
-  {
-    title: "Animation-9",
-    githubLink: "https://github.com/mukh4mmadov/animation-9",
-    vercelLink:
-      "https://animation-9-ejyde7q9o-ozodbeks-projects-5b942123.vercel.app/",
-  },
-  {
-    title: "Animation-10",
-    githubLink: "https://github.com/mukh4mmadov/animation-10",
-    vercelLink: "https://animation-10.vercel.app/",
+    title: "EXAM",
+    githubLink: "https://github.com/mukh4mmadov/my-team1",
+    vercelLink: "https://my-team1-three.vercel.app/",
   },
 ];
 
